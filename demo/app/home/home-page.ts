2f4275c9b43c5eee50f9034bc5dc3d52ed819545
rhanb <aniel.jeanbaptiste@gmail.com>
2019-07-21 20:21:00 +0200

    feat: finalise demo

--- demo/app/home/home-page.ts
+++ demo/app/home/home-page.ts
@@ -1,4 +1,4 @@
-import { ZendeskChatUi } from 'nativescript-zendesk-chat-ui';
+import { ZendeskChatUi } from "nativescript-zendesk-chat-ui";
 /*
 In NativeScript, a file with the same name as an XML file is known as
 a code-behind file. The code-behind is a great place to place your view
@@ -6,11 +6,30 @@ logic, and to set up your page’s data binding.
 */
 
 import { NavigatedData, Page } from "tns-core-modules/ui/page";
+import { isIOS } from "tns-core-modules/platform";
 
 import { HomeViewModel } from "./home-view-model";
+import { key } from "../../../secrets";
 
 export function onNavigatingTo(args: NavigatedData) {
     const page = <Page>args.object;
 
+    ZendeskChatUi.initialize(key);
+
+    if (isIOS) {
+        ZendeskChatUi.chatOverlayEnabled = false;
+    }
+
+    ZendeskChatUi.visitorInfo = {
+        name: "JB",
+        email: "aniel.jeanbaptiste@gmail.com",
+        phoneNumber: "+33600000000",
+        note: "A super nice note"
+    };
+
     page.bindingContext = new HomeViewModel();
+}
+
+export function startChat() {
+    ZendeskChatUi.startChat();
 }
--- demo/app/home/home-view-model.ts
+++ demo/app/home/home-view-model.ts
@@ -1,13 +1,7 @@
 import { Observable } from "tns-core-modules/data/observable";
-import { ZendeskChatUi } from "nativescript-zendesk-chat-ui";
-import { key } from '../../../secrets';
 
 export class HomeViewModel extends Observable {
     constructor() {
         super();
-
-        (ZendeskChatUi as any).initialize(key);
-
-        (ZendeskChatUi as any).startChat();
     }
 }
